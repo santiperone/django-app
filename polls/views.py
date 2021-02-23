d395b6ec4aad64382de3bf4c65cebd95ea6900fe
santiperone <sdperone97@gmail.com>
2021-02-23 18:23:32 -0300

    created poll views templates and logic

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    q = get_object_or_404(Question, id=question_id)
+    return render(request, 'polls/detail.html', {'question': q})
+
+def results(request, question_id):
+    q = get_object_or_404(Question, id=question_id)
+    return render(request, 'polls/results.html', {'question': q})
+
+def vote(request, question_id):
+    q = get_object_or_404(Question, id=question_id)
+    return render(request, 'polls/vote.html', {'question': q})
